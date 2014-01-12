ecac2790ccb9d7014129f135fffa99b8495ad219
arturoagudelo18 <arturoagudelo18@gmail.com>
2014-01-12 23:29:41 +0000

    librerias completas del jasper

--- Inventario/src/inventario/oficinas.java
+++ Inventario/src/inventario/oficinas.java
@@ -1,22 +1,37 @@
 package inventario;
 
 import bd.Conexion;
+import java.awt.HeadlessException;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 public class oficinas extends javax.swing.JFrame {
     Integer indice = 0;
+    Integer idoficina = 0;
     
     public oficinas() {
         initComponents();
         this.setLocationRelativeTo(null);
         dialogoNomina.setLocation(450, 100);
         setResizable(false);
-        setTitle("Oficinas");
+//        setTitle("Oficinas");
         dialogoNomina.setSize(500, 500);
         llenarOficinas();
     }
@@ -28,6 +43,7 @@ public class oficinas extends javax.swing.JFrame {
         dialogoNomina = new javax.swing.JDialog();
         jScrollPane1 = new javax.swing.JScrollPane();
         tablaNomina = new javax.swing.JTable();
+        botonGenerarPDF = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         comboOficinas = new javax.swing.JComboBox();
         btnAtras = new javax.swing.JButton();
@@ -50,22 +66,43 @@ public class oficinas extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tablaNomina);
 
+        botonGenerarPDF.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/Imagenes/documento.png"))); // NOI18N
+        botonGenerarPDF.setText("Generar PDF");
+        botonGenerarPDF.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botonGenerarPDFActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout dialogoNominaLayout = new javax.swing.GroupLayout(dialogoNomina.getContentPane());
         dialogoNomina.getContentPane().setLayout(dialogoNominaLayout);
         dialogoNominaLayout.setHorizontalGroup(
             dialogoNominaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+            .addGroup(dialogoNominaLayout.createSequentialGroup()
+                .addGap(125, 125, 125)
+                .addComponent(botonGenerarPDF, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         dialogoNominaLayout.setVerticalGroup(
             dialogoNominaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(dialogoNominaLayout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 107, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(41, 41, 41)
+                .addComponent(botonGenerarPDF, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 53, Short.MAX_VALUE))
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Bienvenido. Seleccione la oficina"));
+
+        comboOficinas.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "---Seleccione una oficina---" }));
+        comboOficinas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                comboOficinasActionPerformed(evt);
+            }
+        });
 
         btnAtras.setText("Atrás");
         btnAtras.addActionListener(new java.awt.event.ActionListener() {
@@ -129,6 +166,7 @@ public class oficinas extends javax.swing.JFrame {
 
     private void btnAccederActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAccederActionPerformed
         if (comboOficinas.getSelectedIndex() != -1) {
+            llenarNominaPorOficina();
             dialogoNomina.setVisible(true);
         }
     }//GEN-LAST:event_btnAccederActionPerformed
@@ -138,6 +176,37 @@ public class oficinas extends javax.swing.JFrame {
         Administracion administracion = new Administracion();
         administracion.setVisible(true);
     }//GEN-LAST:event_btnAtrasActionPerformed
+
+    private void comboOficinasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboOficinasActionPerformed
+        getIdOficina();
+    }//GEN-LAST:event_comboOficinasActionPerformed
+
+    private void botonGenerarPDFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonGenerarPDFActionPerformed
+        Connection conexion = null;
+        try {
+            try {
+                Class.forName("org.postgresql.Driver");
+                conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/inventario", "postgres", "r00t");
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            
+            
+            File file = new File("reporte.jasper");
+            Map<String, Object> mapa = new HashMap<>();
+            mapa.put("nombre", "Carlos");
+            JasperReport reporte = (JasperReport) JRLoader.loadObject(file);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reporte, null, new JREmptyDataSource());
+            JasperViewer visor = new JasperViewer(jasperPrint);
+            JOptionPane.showConfirmDialog(rootPane, visor);
+        } catch (HeadlessException | JRException ex) {
+            JOptionPane.showMessageDialog(rootPane, "Ha ocurrido una excepcion creando el reporte","Error",JOptionPane.ERROR_MESSAGE);
+            ex.printStackTrace();
+        }
+        
+    }//GEN-LAST:event_botonGenerarPDFActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -171,6 +240,7 @@ public class oficinas extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton botonGenerarPDF;
     private javax.swing.JButton btnAcceder;
     private javax.swing.JButton btnAtras;
     private javax.swing.JComboBox comboOficinas;
@@ -195,6 +265,58 @@ public class oficinas extends javax.swing.JFrame {
             
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(rootPane, "Ha ocurrido un error consultando las oficinas","Error",JOptionPane.ERROR_MESSAGE);
+            ex.printStackTrace();
+        }
+    }
+
+    private void llenarNominaPorOficina() {
+        Object datos[]=new Object[3]; 
+        DefaultTableModel defaultTableModel;
+        try {
+            defaultTableModel = new DefaultTableModel(null, getColumnas());
+            tablaNomina.setModel(defaultTableModel);
+            Statement stmt = Conexion.Conectarse().createStatement();
+            Statement stmt2 = Conexion.Conectarse().createStatement();
+            String consulta = "SELECT e.nombre,e.apellidos,e.cargo from empleado e WHERE e.oficina = "+idoficina+"";
+            String consultaTitulo = "SELECT o.nombre from oficinas o WHERE o.id = "+idoficina+"";
+            ResultSet rs = stmt.executeQuery(consulta);
+            ResultSet rs2 = stmt2.executeQuery(consultaTitulo);
+            String titulo = "";
+            while (rs2.next()) {
+                titulo = rs2.getString("nombre");
+            }
+            while (rs.next()) {
+                for (int i = 0; i < 3; i++) {
+                    datos[i] = rs.getObject(i + 1);
+                }
+                defaultTableModel.addRow(datos);
+            }
+            rs.close();
+            rs2.close();
+            
+            dialogoNomina.setTitle("Empleados de la oficina de "+titulo);
+            rs.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(rootPane, "Ha ocurrido un error consultando los empleados","Error",JOptionPane.ERROR_MESSAGE);
+            ex.printStackTrace();
+        }
+    }
+    private String[] getColumnas(){
+        String columna[]=new String[]{"Empleado","Apellido","Cargo"};
+        return columna;
+    }
+
+    private void getIdOficina() {
+        try {
+            Statement stmt = Conexion.Conectarse().createStatement();
+            ResultSet resulset = stmt.executeQuery("SELECT id FROM oficinas WHERE nombre = '"+comboOficinas.getItemAt(comboOficinas.getSelectedIndex())+"'");
+            while (resulset.next()) {
+                idoficina = resulset.getInt("id");
+            }
+            System.out.println("ID Oficina--> "+idoficina);
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(rootPane, "Ha ocurrido un error cargando el id de la oficina","Error",JOptionPane.ERROR_MESSAGE);
             ex.printStackTrace();
         }
     }
